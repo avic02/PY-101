e8913483be767a64abb994dc29b2c197660b1c9a
Avi <avichoudhary559@gmail.com>
2025-08-13 13:27:18 +0530

    typo fix

--- DAY3/Treasure_hunt.py
+++ DAY3/Treasure_hunt.py
@@ -26,7 +26,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island of Dungeons and Dragons .")
 
 print("Your mission is to find the treasure and hopefully be alive in one piece.\n\nLET THE GAMES BEGIN CHILD!") 
-lvl_diff = input("choose level of difficulty : Harder / Harder / Hardest: H1 /H2 /H3?\n")
+lvl_diff = input("choose level of difficulty : Hard / Harder / Hardest: H1 /H2 /H3?\n")
 
 if lvl_diff == "hardest" or lvl_diff == "H3":
           print("You are the choosen one please enter .\n THE ONLY RULE IS DONT DIE ")
@@ -40,7 +40,7 @@ Location = Location.lower()
 if Location == "rural":
           choice_1 = input("choose LAND OR SEA : L OR S ?")
           if choice_1 == "S":
-                    print("You just encountered with GHOST OF SALTMARSH \n You are                                      dedddhhh ")
+                    print("You just encountered with GHOST OF SALTMARSH \n You are dedddhhh ")
           if choice_1 == "L":
                     choice_2 = input("choose to enter Zombies or vampire camp :Z / V ?")
                     if choice_2 == "Z":
@@ -48,26 +48,26 @@ if Location == "rural":
                               choice_3 = input("would you fight them or run : F / R?")
                               
                               if choice_3 == "F":
-                                        print("you choose to fight the zombies but got                                                             outnumberd .YOU ARE DEDHHHHH!")
+                                        print("you choose to fight the zombies but got outnumberd .YOU ARE DEDHHHHH!")
                                         
                               elif choice_3 == "R": 
-                                        print("You escaped and came across a treasure                                                             chest : what wld you do ?")
+                                        print("You escaped and came across a treasure chest : what wld you do ?")
                                         choice_4 = input("open or leave game :")
                                         
                                         if choice_4 == "open" or choice_4 == "O" :
-                                                  print("There was a zombie siting inside                                     the dummy chest .He bit you \n YOU ARE DEADDDDDHHH.")
+                                                  print("There was a zombie siting inside the dummy chest .He bit you \n YOU ARE DEADDDDDHHH.")
                                                   
                                         if choice_4 == "leave" or choice_4 == "L":
-                                                  print("You came across a princess who                                                                       had the treasure                               chest . She asked you to choose a number btw 1 and 100 .")
+                                                  print("You came across a princess who had the treasure chest . She asked you to choose a number btw 1 and 100 .")
                                                   
-                                        choice_5 = int(input("enter a number btw 1 and                                                                       100:"))
+                                        choice_5 = int(input("enter a number btw 1 and 100:"))
                                         
                                         if choice_5 > 50 and choice_5 < 90 : 
-                                                  print("YOU WON CONGRATULATIONS ON                                                                       WASTING YOUR TIME")
+                                                  print("YOU WON CONGRATULATIONS ON WASTING YOUR TIME")
                                         
                                         
                     elif choice_2 == "V":
-                              print("You entered vampire's cave and he sucked the blood                                                   out of you . YOU ARE DEADDDDDHHHH!!  ")
+                              print("You entered vampire's cave and he sucked the blood out of you . YOU ARE DEADDDDDHHHH!!  ")
                     else : 
                               print("WRONG INPUT ENTERED. RETRY ")
 
